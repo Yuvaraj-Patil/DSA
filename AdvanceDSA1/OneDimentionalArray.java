d662c4abf0e75c7b3835ecce2ac981080407faf4
Yuvaraj-Patil <you@example.com>
2024-09-23 23:48:09 +0530

    Problem Statement Added

--- AdvanceDSA1/OneDimentionalArray.java
+++ AdvanceDSA1/OneDimentionalArray.java
@@ -6,6 +6,13 @@ public class OneDimentionalArray
     {
         BeggersProblem();
     }
+    /*There are A beggars sitting in a row outside a temple. Each beggar initially has an empty pot. 
+    When the devotees come to the temple, they donate some amount of coins to these beggars. Each 
+    devotee gives a fixed amount of coin(according to their faith and ability) to some K beggars sitting 
+    next to each other. Given the amount P donated by each devotee to the beggars ranging from L to R index, 
+    where 1 <= L <= R <= A, find out the final amount of money in each beggar's pot at the end of the day, 
+    provided they don't fill their pots by any other means. For ith devotee B[i][0] = L, B[i][1] = R, B[i][2] = P, 
+    given by the 2D array B*/
     private static void BeggersProblem()
     {
         int A = 5;
