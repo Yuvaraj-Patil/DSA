4c3e4cb529ac361ec6812632479d79066db12524
Yuvaraj-Patil <yuvarajpatil79@gmail.com>
2024-10-07 17:18:24 +0530

    Recursion Changes

--- Recursion.java
+++ Recursion.java
@@ -1,32 +1,73 @@
-public class Recursion{
-    public static void Main(String[] args)
+public class Recursion
+{
+    public static void main(String[] args)
     {
         System.out.println(Sum(8));
         System.out.println(Fact(10));
-        Increasing(9);
+        increasing(9);
+        System.out.println();
+        decreasing(9);
+        System.out.println();
+        DecThenInc(16);
+        System.out.println();
         System.out.println(power(2,10));
         int[] arr={10,20,30,40,50};
         printArray(arr,0);
-        fibonaccii(12);
+        System.out.println(fibonaccii(12));
+        System.out.println(checkPalindrome("ana", 0, 2));
+        int[] ans=findAll(new int[]{2,5,7,1,5,3,5},5,0,0);
+        for (int i : ans) 
+        {
+            System.out.print(i+" ");
+        }
     }
+
+    /*
+     * 
+     */
     public static int Sum(int n)
     {
         if(n==0)
             return 0;
         return Sum(n-1)+n;
     }
+
+    /*
+     * Write a program to find the factorial of the given number A using recursion.
+     */
     public static int Fact(int n)
     {
         if(n==0)
             return 1;
         return Fact(n-1)*n;
     }
-    public static void Increasing(int A)
+
+    /*
+     * You are given an integer A, print 1 to A using using recursion.
+     */
+    public static void increasing(int A)
     {
-        if(A==0)
+        if(A==1)   
+        {         
+            System.out.print(A+" ");
             return;
-        Increasing(A-1);
-        System.out.print(A+" ");
+        }       
+        increasing(A-1);        
+        System.out.print(A+" "); 
+    }
+
+    /*
+     * You are given an integer A, print A to 1 using using recursion.
+     */
+    public static void decreasing(int A)
+    {
+        if(A==1)   
+        {         
+            System.out.println(A+" ");
+            return;
+        }
+        System.out.print(A+" ");        
+        decreasing(A-1);         
     }
 
     public static int power(int a,int n)
@@ -50,18 +91,69 @@ public class Recursion{
         printArray(a,index+1);
     }
 
+    /*
+     * The Fibonacci numbers are the numbers in the following integer sequence.
+     * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+     * In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation:
+     * Fn = Fn-1 + Fn-2
+     * Given a number A, find and return the Ath Fibonacci Number using recursion.
+     * Given that F0 = 0 and F1 = 1.
+     */
     public static int fibonaccii(int n)
     {
         if(n<2)
             return n;
         return fibonaccii(n-1)+fibonaccii(n-1);
     }
-    // public static int[] findAll(int[] A,int B,int index, int count)
-    // {
-    //     if(index==A.length)
-    //         return new int[count];
-    //     if(A[index]==B)
-    //         count++;
-    //     int[] ans=findAll(A,B,index+1,count);        
-    // }
+
+    /*
+     * Given a number A, we need to find the sum of its digits using recursion.
+     */
+    public int sumOfDigitsOfA(int A) {
+        if(A==0)
+            return 0;        
+        return A%10+sumOfDigitsOfA(A/10);
+    }
+
+    /*
+     * Print N numbers in Decreasing Order and then in Increasing Order.
+     * You are given a positive number N.
+     * You are required to print the numbers from N to 1.
+     * You are required to not use any loops. 
+     */
+    public static void DecThenInc(int A)
+    {
+        if(A==0)
+            return;
+        System.out.print(A+" ");
+        DecThenInc(A-1);
+        System.out.print(A+" ");
+    }
+
+
+    public static int[] findAll(int[] A,int B,int index, int count)
+    {
+        if(index==A.length)
+            return new int[count];
+        if(A[index]==B)
+        {
+            int arr[]=findAll(A,B,index+1,count+1);
+            arr[count]=index;
+            return arr;
+        }
+        else
+        {
+            int arr[]= findAll(A,B,index+1,count);   
+            return arr;
+        }     
+    }
+
+    public static boolean checkPalindrome(String str,int s,int e)
+    {
+        if(s>e)
+            return true;
+        if(Character.compare(str.charAt(s),str.charAt(e))!=0)
+            return false;
+        return checkPalindrome(str, s+1, e-1);
+    }
 }
